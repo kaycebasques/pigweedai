2f504b1dd07d39d7dd10d378fb9bb7de094c1458
Kayce Basques <kaycebasques@gmail.com>
2023-05-31 16:29:59 -0700

    Fix URL

--- src/sphinx/palmweed.js
+++ src/sphinx/palmweed.js
@@ -60,7 +60,7 @@ function generateResponse() {
     headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
     body: JSON.stringify({'query': getQuery()})
   };
-  fetch('/api/query', options)
+  fetch('https://server-ic22qaceya-uc.a.run.app/api/query', options)
       .then(response => response.json())
       .then(json => render(json));
 }
