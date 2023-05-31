9a2f8ee871de18cff1575e74511d10eeb82f2e8a
Kayce Basques <kaycebasques@gmail.com>
2023-05-31 08:42:23 -0700

    Try to load data from JSON

--- src/firebase/functions/main.py
+++ src/firebase/functions/main.py
@@ -4,9 +4,14 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from json import load
 
 initialize_app()
 
+with open('test.json', 'r') as f:
+    data = load(f)
+
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello, Palmweed!")
+    message = data['message']
+    return https_fn.Response(message)
