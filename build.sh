b9f26a8bf4794e0cd3b93183c6d60443757dd545
Kayce Basques <kaycebasques@gmail.com>
2023-05-31 10:51:07 -0700

    Try escaping all the things

--- build.sh
+++ build.sh
@@ -6,22 +6,26 @@ function bootstrap_docs_repo() {
     cd ..
 }
 
+# This function just updates a couple lines in the configuration files
+# of //pigweed. If you run this function and then `cd pigweed && git diff`
+# you can see the changes. There are probably more readable ways to do this
+# but this approach is good enough for now.
 function hack_palmweed_into_docs_repo() {
-    BEFORE="'pw_docgen.sphinx.module_metadata',"
-    AFTER="$BEFORE\n    'pw_docgen.sphinx.palmweed',"
+    BEFORE="\'pw_docgen\.sphinx\.module_metadata\',"
+    AFTER="$BEFORE\n    \'pw_docgen\.sphinx\.palmweed\',"
     sed -i "s/$BEFORE/$AFTER/" pigweed/docs/conf.py
     cp src/sphinx/palmweed.py pigweed/pw_docgen/py/pw_docgen/sphinx/palmweed.py
  
-    BEFORE="\"pw_docgen\/sphinx\/module\_metadata.py\","
-    AFTER="$BEFORE\n    \"pw_docgen\/sphinx\/palmweed.py\","
+    BEFORE="\"pw_docgen\/sphinx\/module\_metadata\.py\","
+    AFTER="$BEFORE\n    \"pw_docgen\/sphinx\/palmweed\.py\","
     sed -i "s/$BEFORE/$AFTER/" pigweed/pw_docgen/py/BUILD.gn
  
-    BEFORE="html_static_path = ['docs/_static']"
-    AFTER="$BEFORE\nhtml_js_files = ['palmweed.js']"
+    BEFORE="html_static_path = \[\'docs\/_static\'\]"
+    AFTER="$BEFORE\nhtml_js_files = \[\'palmweed\.js\'\]"
     sed -i "s/$BEFORE/$AFTER/" pigweed/pw_docgen/py/BUILD.gn
 
-    BEFORE="\"\_static\/css\/pigweed.css\","
-    AFTER="$BEFORE\n    \"\_static\/palmweed.js\","
+    BEFORE="\"\_static\/css\/pigweed\.css\","
+    AFTER="$BEFORE\n    \"\_static\/palmweed\.js\","
     sed -i "s/$BEFORE/$AFTER/" pigweed/docs/BUILD.gn
     cp src/sphinx/palmweed.js pigweed/docs/_static/palmweed.js
 }
@@ -39,4 +43,4 @@ function build_docs() {
 
 bootstrap_docs_repo
 hack_palmweed_into_docs_repo
-build_docs
+# build_docs
