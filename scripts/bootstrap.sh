8cbca46825d52a2819b5ac5ed9b17b8ef8b0cdd7
Kayce Basques <kaycebasques@gmail.com>
2023-06-06 13:48:23 -0700

    Fix glue stuff

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,7 +19,9 @@ function bootstrap_firebase() {
     cd functions
     python3 -m venv venv
     source venv/bin/activate
-    python3 pip install -r requirements.txt
+    python3 -m pip install -r requirements.txt
+    deactivate
+    cd ../../..
 }
 
 bootstrap_docs_repo
