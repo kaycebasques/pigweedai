d78f7d5b4a02f622d55cb2eaa182394ccaa9ab34
Kayce Basques <kaycebasques@gmail.com>
2023-06-05 17:58:22 -0700

    Refactor to use OpenAI

--- src/firebase/functions/main.py
+++ src/firebase/functions/main.py
@@ -1,93 +1,87 @@
 from firebase_functions import https_fn
 from firebase_functions.options import MemoryOption
-from firebase_admin import initialize_app, firestore as firestore_init, credentials
-from firebase_admin.exceptions import FirebaseError
+from firebase_admin import initialize_app, firestore, credentials
 from json import load, dumps
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-import google.generativeai as palm
 from datetime import datetime
-from time import sleep, time
+from time import time
 import numpy as np
-from traceback import print_exc
 from markdown import markdown
 from hashlib import md5
 from math import ceil
-from xml.sax.saxutils import escape
+import openai
+from tiktoken import get_encoding
 
 database = {}
-embedding_model = 'models/embedding-gecko-001'
-chat_model = 'models/text-bison-001' # TODO: Should be chat-bison
-# palm.list_models() shows that this is the limit for models/embedding-gecko-001.
-token_limit = 1024 # TODO: Is this needed anymore?
 with open('env.json', 'r') as f:
     env = load(f)
 service_account_credentials = credentials.Certificate('service_account.json')
 initialize_app(service_account_credentials)
-firestore = firestore_init.client()
-embeddings = firestore.collection('embeddings')
-chats = firestore.collection('chats')
+firestore_client = firestore.client()
+embeddings = firestore_client.collection('embeddings')
+chats = firestore_client.collection('chats')
 app = Flask(__name__)
 CORS(app)
-palm.configure(api_key=env['palm'])
+openai.api_key = env['openai']
+openai_embedding_model = 'text-embedding-ada-002'
+openai_encoder = get_encoding('cl100k_base')
 # Load the Firestore embeddings data to the in-memory "database".
 docs = embeddings.stream()
 for doc in docs:
-    doc_data = doc.to_dict()
+    data = doc.to_dict()
     checksum = doc.id
-    database[checksum] = doc_data
-# TODO: Create logic to remove entries that have not been timestamped for one month.
-# TODO: Call the retry function.
-
-# General utility functions.
-def handle_exception(exception):
-    print(f'Exception: {exception}')
-    print('Exception stack trace:')
-    print_exc()
-    return {'error': 'Something broke...'}
+    if 'openai' not in data or 'openai_token_count' not in data:
+        continue
+    if data['openai'] is None or data['openai_token_count'] is None:
+        continue
+    # Don't load the other data until it's needed.
+    database[checksum] = {
+        'openai': data['openai'],
+        'openai_token_count': data['openai_token_count']
+    }
 
 def get_timestamp():
     return ceil(time())
 
-def retry_embedding_generation():
-    pass
-
-def closest(target):
-    # Calculate how far each docs section is from the target query.
-    calculations = []
+def find_relevant_docs(target):
+    distances = []
     for checksum in database:
-        section = database[checksum]
-        if 'embedding' not in section:
-            continue
-        candidate = section['embedding']
+        doc = database[checksum]
+        candidate = doc['openai']
+        token_count = doc['openai_token_count']
         distance = np.dot(target, candidate)
-        calculations.append({
+        distances.append({
+            'checksum': checksum,
             'distance': distance,
-            'text': section['text'],
-            'token_count': section['token_count'],
-            'path': section['path']
+            'token_count': token_count
         })
-    calculations = sorted(calculations, key=lambda item: item['distance'])
-    calculations.reverse()
-    tokens = 0
-    # Keep this below the model's maximum limit so that there is space for the user's
-    # query and for the prompt instructions.
-    limit = 5000
-    matches = []
-    for item in calculations:
-        if tokens > limit:
+    distances = sorted(distances, key=lambda item: item['distance'])
+    distances.reverse()
+    current_token_count = 0
+    token_limit = 1000
+    results = []
+    for item in distances:
+        checksum = item['checksum']
+        if current_token_count > token_limit:
             continue
-        if tokens + item['token_count'] > limit:
+        if current_token_count + item['token_count'] > token_limit:
             continue
-        matches.append(item)
-        tokens += item['token_count']
-    # TODO: Return all matches, not just the first one.
-    return matches
+        if 'text' not in database[checksum]:
+            ref = embeddings.document(checksum)
+            doc = ref.get()
+            data = doc.to_dict()
+            database[checksum] = data
+        clone = dict(database[checksum])
+        clone.pop('openai')
+        results.append(clone)
+        current_token_count += item['token_count']
+    return results
 
 @app.get('/debug')
 def debug():
-    response = palm.chat(messages='Hello!')
-    return response.last
+    print(database)
+    return {'ok': True}
 
 def update_chat_logs(uuid, author, message, context=None):
     ref = chats.document(uuid)
@@ -105,13 +99,20 @@ def update_chat_logs(uuid, author, message, context=None):
     pass
 
 def create_context(message):
-    embedding = palm.generate_embeddings(text=message,
-            model=embedding_model)['embedding']
-    data = closest(embedding)
-    context = [item['text'] for item in data]
-    context = ' '.join(context)
-    context = f'Use this information in your answer: {context}'
-    context = context.replace('\n', ' ')
+    embedding = create_openai_embedding(message)
+    data = find_relevant_docs(embedding)
+    sections = [item['text'] for item in data]
+    sections = ''.join(sections)
+    document = f'<document>{sections}</document>'
+    question = f'<question>{message}</question>'
+    instructions = [
+        'Help the user develop an embedded system with Pigweed.',
+        'Answer the following question:',
+        question,
+        'Use information from the following Docutils document tree in your answer.',
+        document
+    ]
+    context = ' '.join(instructions)
     return context
 
 @app.post('/chat')
@@ -120,73 +121,51 @@ def chat():
         data = request.get_json()
         message = data['message']
         uuid = data['uuid']
-        context = create_context(message)
+        # context = create_context(message)
         # TODO: Escape the JSON string.
         # response = palm.generate_text(prompt=dumps(context), temperature=0)
         # reply = response.result
         model = 'models/chat-bison-001'
-        response = palm.chat(messages=message, context=context, temperature=0, model=model)
-        # response = palm.chat(messages=message, temperature=0, model=model)
+        # response = palm.chat(messages=message, context=context, temperature=0, model=model)
+        response = palm.chat(messages=message, temperature=0, model=chat_model)
         reply = response.last
         html = markdown(reply, extensions=['markdown.extensions.fenced_code'])
         return {
             'reply': html
         }
     except Exception as e:
-        return handle_exception(e)
+        return {'ok': False}
 
-def embed(text):
-    response = palm.generate_embeddings(text=text, model=embedding_model)
-    embedding = response['embedding']
-    return response['embedding']
+def create_openai_embedding(text):
+    response = openai.Embedding.create(input=[text], model=openai_embedding_model)
+    return response['data'][0]['embedding']
 
-@app.post('/generate_embedding')
-def generate_embedding():
-    try:
-        print('/generate_embedding')
-        request_data = request.get_json()
-        text = request_data['text']
-        token_count = request_data['token_count']
-        path = request_data['path']
-        checksum = md5(text.encode('utf-8')).hexdigest()
-        print(checksum)
-        print(path)
-        firebase_ref = embeddings.document(checksum)
-        firebase_doc = firebase_ref.get()
-        if firebase_doc.exists and 'embedding' in firebase_doc.to_dict():
-            firebase_data = firebase_doc.to_dict()
-            # Refresh the timestamp to indicate that this text is still fresh.
-            firebase_data['timestamp'] = get_timestamp()
-            firebase_ref.set(firebase_data)
-            return {'embedding': firebase_doc.to_dict()['embedding']}
-        new_data = {
-            'text': text,
-            'token_count': token_count,
-            'path': path,
-            'timestamp': get_timestamp()
-        }
-        # Save early because the the embedding API often fails.
-        # Other parts of the server codebase try to complete the embedding
-        # process later.
-        firebase_ref.set(new_data)
-        embedding = embed(text)
-        new_data['embedding'] = embedding
-        firebase_ref.set(new_data)
-        return {'embedding': embedding}
-    except Exception as e:
-        return handle_exception(e)
+def get_openai_token_count(text):
+    return len(openai_encoder.encode(text))
 
-@app.post('/count_tokens')
-def count_tokens():
-    try:
-        print('/count_tokens')
-        body = request.get_json()
-        text = body['text']
-        response = palm.count_message_tokens(prompt=text)
-        return {'token_count': response['token_count']}
-    except Exception as e:
-        return handle_exception(e)
- 
+@app.post('/create_embedding')
+def create_embedding():
+    print('POST /create_embedding')
+    data = request.get_json()
+    text = data['text']
+    checksum = md5(text.encode('utf-8')).hexdigest()
+    data['timestamp'] = get_timestamp()
+    ref = embeddings.document(checksum)
+    firestore_doc = ref.get()
+    firestore_data = firestore_doc.to_dict() if firestore_doc.exists else None
+    openai_embedding = None
+    openai_token_count = None
+    ok = firestore_data is None or \
+            firestore_data['openai'] is None or \
+            firestore_data['openai_token_count'] is None
+    if not ok:
+        openai_embedding = create_openai_embedding(text)
+        openai_token_count = get_openai_token_count(text)
+    data['openai_token_count'] = openai_token_count
+    data['openai'] = openai_embedding
+    ref.set(data)
+    return {'ok': True}
+
 @https_fn.on_request(timeout_sec=120, memory=MemoryOption.MB_512)
 def server(req: https_fn.Request) -> https_fn.Response:
     with app.request_context(req.environ):
--- src/sphinx/embeddings.py
+++ src/sphinx/embeddings.py
@@ -12,120 +12,52 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-from docutils import nodes
+from docutils.nodes import section
 from hashlib import md5
-from json import dump, load, dumps
+from json import dumps
 from copy import deepcopy
-from os.path import exists, join
-from os import makedirs, remove
-from glob import glob
-from requests import post
-from math import floor, ceil
-from time import sleep
+from os import getcwd
+from requests import post, get
 
-# TODO: Explain how this relates to the embedding model and chunking logic.
-token_limit = 500
 server = 'https://server-ic22qaceya-uc.a.run.app'
+if 'kayce' in getcwd():
+    server = 'http://127.0.0.1:5001/palmweed-prototype/us-central1/server'
+create_embedding_url = f'{server}/create_embedding'
 
-def split_doc_into_embeddable_chunks(doc, doc_size_in_tokens):
-    num_of_chunks = ceil(doc_size_in_tokens / token_limit)
-    chunk_size_in_tokens = ceil(doc_size_in_tokens / num_of_chunks)
-    len_of_doc_in_chars = len(doc)
-    chunk_size_in_chars = floor(len_of_doc_in_chars / num_of_chunks)
-    chunk_start_index = 0
-    chunk_end_index = chunk_size_in_chars
-    chunks = []
-    while chunk_end_index < len_of_doc_in_chars:
-        chunk = doc[chunk_start_index:chunk_end_index]
-        token_count = get_token_count(chunk)
-        if token_count > 250:
-            chunks.append({
-                'text': chunk,
-                'token_count': token_count
-            })
-        chunk_start_index += chunk_size_in_chars
-        chunk_end_index += chunk_size_in_chars
-        # Termination case.
-        if chunk_end_index > len_of_doc_in_chars:
-            chunk = doc[chunk_start_index:chunk_end_index]
-            token_count = get_token_count(chunk)
-            if token_count > 250:
-                chunks.append({
-                    'text': chunk,
-                    'token_count': token_count
-                })
-    return chunks
+def init(app):
+    # Throws an unhandled exception if the server isn't available.
+    get(server)
 
-def get_token_count(text):
-    try:
-        url = f'{server}/count_tokens'
-        data = {'text': text}
-        headers = {'Content-Type': 'application/json'}
-        response = post(url, data=dumps(data), headers=headers)
-        return response.json()['token_count']
-    except Exception as e:
-        return None
+def find_title(doctree):
+    for node in doctree.traverse():
+        if node.tagname == 'title':
+            return node.astext()
 
-# TODO: Use doctree.get('title') to get document title.
-def generate_embeddings_for_docs(app, doctree, docname):
-    clone = deepcopy(doctree)
-    doc_text = clone.astext()
-    # if 'pw_rpc' not in doc_text:
-    #     return
-    doc_token_count = get_token_count(doc_text)
-    if doc_token_count is None:
+def create_embedding(text, token_count, title, url):
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        'text': text,
+        'title': title,
+        'url': url
+    }
+    post(create_embedding_url, data=dumps(data), headers=headers)
+
+def create_embeddings(app, doctree, docname):
+    if 'pw_rpc' not in docname:
         return
-    chunks = split_doc_into_embeddable_chunks(doc_text, doc_token_count)
-    url = f'{server}/generate_embedding'
-    for chunk in chunks:
-        headers = {'Content-Type': 'application/json'}
-        data = {
-            'text': chunk['text'],
-            # TODO: Create URL instead.
-            'path': app.builder.get_target_uri(docname),
-            'token_count': chunk['token_count']
-        }
-        # This is a blocking call. We don't care about the response so maybe
-        # we should use a non-blocking alternative in the future. It would have
-        # to be another library because requests only offers the blocking API.
-        # For now, it's probably good to slow down these requests because the
-        # embedding API has a rate limit of 300 requests per minute.
-        post(url, data=dumps(data), headers=headers)
-        sleep(1) # Also to help stay within the rate limits.
-
-def generate_embeddings_for_headers(app, exception):
-    # This is a silly hack to reach allllllllll the way back to the real
-    # Pigweed repo root directory and find the headers there.
-    headers = glob(f'{app.srcdir}/../../../../../pw_*/public/**/*.h', recursive=True)
-    # with open(f'{app.outdir}/headers.json', 'w') as f:
-    #     dump({'headers': headers}, f, indent=4)
-    url = f'{server}/generate_embedding'
-    for header in headers:
-        if 'internal' in header:
+    clone = deepcopy(doctree)
+    title = find_title(clone)
+    url = f'https://pigweed.dev/{app.builder.get_target_uri(docname)}'
+    for node in clone.traverse(section):
+        try:
+            xml = node.asdom().toxml()
+            create_embedding(xml, title, url)
+        except Exception as e:
             continue
-        with open(header, 'r') as f:
-            doc_text = f.read()
-        # if 'pw_rpc' not in doc_text:
-        #     continue
-        doc_token_count = get_token_count(doc_text)
-        if doc_token_count is None:
-            return
-        chunks = split_doc_into_embeddable_chunks(doc_text, doc_token_count)
-        for chunk in chunks:
-            headers = {'Content-Type': 'application/json'}
-            target = '/../../../../..'
-            index = header.index(target) + len(target)
-            path = header[index:]
-            data = {
-                'text': chunk['text'],
-                'path': path,
-                'token_count': chunk['token_count']
-            }
-            post(url, data=dumps(data), headers=headers)
 
 def setup(app):
-    app.connect('doctree-resolved', generate_embeddings_for_docs)
-    # app.connect('build-finished', generate_embeddings_for_headers)
+    app.connect('builder-inited', init)
+    app.connect('doctree-resolved', create_embeddings)
     return {
         'version': '0.0.0',
         'parallel_read_safe': True,
